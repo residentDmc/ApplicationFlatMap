2ae7a494a6f439f40a8cd10fd6e9eb099003b869
Sajad <sajad.developer.70@gmail.com>
2019-09-04 16:40:54 +0430

    com

---
+++ app/src/main/java/com/example/applicationflatmap/UploadFileActivity.java
@@ -0,0 +1,24 @@
+package com.example.applicationflatmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class UploadFileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_file);
+    }
+
+    public void startService(View view) {
+        startService(new Intent(this, MyService.class));
+    }
+    // Stop the service
+    public void stopService(View view) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
